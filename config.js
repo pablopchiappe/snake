29c7f3ef547b2b970658b69dbecfebd86352afee
Palo22 <pablo.p.chiappe@gmail.com>
2026-04-23 16:34:08 -0300

    v1.2 minor changes

--- config.js
+++ config.js
@@ -1,5 +1,8 @@
 // config.js
+
+window.APP_VERSION = "v1.2";
+
 window.env = {
   SUPABASE_URL: "https://bbupvsdjgsmwznuumfbk.supabase.co",
-  SUPABASE_KEY: "sb_publishable_1vH0UCb6EDpwvLaYWDQkpw_kL89ZkqP"
-};
+  SUPABASE_KEY: "sb_publishable_1vH0UCb6EDpwvLaYWDQkpw_kL89ZkqP",
+ };
